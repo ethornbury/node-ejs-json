4303cd81f9f8a9a5bae9900ee29c5a8ba49f70d4
ethornbury <thornburye@gmail.com>
2019-03-02 15:28:51 +0000

    file uploads a jpg to the public/photo-storage folder, uses multer

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ app.set("view engine", "ejs"); // This line sets the default view wngine
 
 var http = require('http');
 var bodyParser = require("body-parser");
+const multer = require('multer'); // file storing middleware
+//var formidable = require('formidable');
 var fs = require('fs');
-
+app.use(bodyParser.urlencoded({extended:false})); //handle body requests
 
 app.use(express.static("views"));       // Allow access to content of views folder
 app.use(express.static("scripts"));     // Allow access to scripts folder
@@ -52,8 +54,48 @@ app.get('/about', function(req, res) {
 	console.log("about page now rendered");
 });
 
+const multerConfig = {
+    
+storage: multer.diskStorage({
+ //Setup where the user's file will go
+ destination: function(req, file, next){
+   next(null, './public/photo-storage');
+   },   
+    
+    //Then give the file a unique name
+    filename: function(req, file, next){
+        console.log(file);
+        const ext = file.mimetype.split('/')[1];
+        next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+      }
+    }),   
+    
+    //A means of ensuring only images are uploaded. 
+    fileFilter: function(req, file, next){
+          if(!file){
+            next();
+          }
+        const image = file.mimetype.startsWith('image/');
+        if(image){
+          console.log('photo uploaded');
+          next(null, true);
+        }else{
+          console.log("file not supported");
+          
+          //TODO:  A better message response to user on failure.
+          return next();
+        }
+    }
+  };
+  
 
-
+app.get('/upload', function(req, res) {
+	res.render('upload');
+	console.log("upload page now rendered");
+});
+app.post('/upload',multer(multerConfig).single('photo'),function(req,res){
+   res.send('Complete!');
+});
 
   // This function gets the application up and running on the development server.
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
